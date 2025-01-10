64030ca3736bfc959613b3aeaf258ce20c14401b
satyamkant <satyamkant2805@gmail.com>
2025-01-10 14:39:48 +0530

    adding userMs and securityMs

---
+++ securityms/src/test/java/com/validators/securityms/SecuritymsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validators.securityms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritymsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
